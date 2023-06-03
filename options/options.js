ce1a35c20b14f9ce3fd5eb7bcb697275f206c210
Rich Flores <Richardflores0199@gmail.com>
2023-06-02 18:35:03 -0700

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "pure-blood-41206",
-  url: "https://pure-blood-41206.botics.co",
-  api: "https://pure-blood-41206.botics.co/api/v1"
-}
+  "name": "pure-blood-41206",
+  "url": "https://pure-blood-41206.botics.co",
+  "api": "https://pure-blood-41206.botics.co/api/v1",
+  "initialRoute": "signup"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
