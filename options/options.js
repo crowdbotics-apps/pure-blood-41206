da328eb905f3e3c2578130bbf5ca7b15734c12e3
Crowdbotics <team@crowdbotics.com>
2023-06-03 01:27:20 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "pure-blood-41206",
+  url: "https://pure-blood-41206.botics.co",
+  api: "https://pure-blood-41206.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
